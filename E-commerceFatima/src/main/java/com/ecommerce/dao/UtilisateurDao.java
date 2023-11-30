1f8f1cec10942488c52215974e0ace6b11132e11
gitfatimae <fatimaezzahraetaibi88@gmail.com>
2023-11-30 10:18:15 +0100

    tp4E-com

--- E-commerceFatima/src/main/java/com/ecommerce/dao/UtilisateurDao.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.dao;
-
-import com.ecommerce.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UtilisateurDao extends CrudRepository<User, String> {
-}
